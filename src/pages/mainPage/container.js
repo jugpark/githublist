bed02e5bffbebf30bf9d791d50775bdbba4f987e
jugwang park <90168987+jugpark@users.noreply.github.com>
2023-01-17 13:45:58 +0900

    검색한 repository 등록 및 삭제 외 localstorage 에 저장

--- src/pages/mainPage/container.js
+++ src/pages/mainPage/container.js
@@ -5,25 +5,35 @@ import { octokit } from "../../authentication/authentication";
 
 const Container = (props) => {
     const [searchObj, setSearchObj] = useState({
-        owner: null,
-        repo: null,
+        // owner: null,
+        // repo: null,
+        owner: "octokit",
+        repo: "rest.js",
     });
     const [searchedRepo, setSearchedRepo] = useState([])
-    const [registeredRepo, setRegisterdRepo] = useState([]);
+    const [registeredRepo, setRegisterdRepo] = useState(JSON.parse(localStorage.getItem("registeredRepo")));
 
-    // owner: "octocat",
-    // repo: "Spoon-Knife",
+    console.log(registeredRepo)
 
-    console.log(searchObj)
-    console.log(searchedRepo)
-
+    //검색된 레포지토리가 있는지 확인
     const repositoryFetch = async () => {
-        await octokit.request(`GET /repos/${searchObj.owner}/${searchObj.repo}/issues`).then((res) => {
-            setSearchedRepo(res.data)
-            console.log(res.data)
-        }).catch((error) => {
-            console.log(error.response.data.message)
-        })
+        // await octokit.request(`GET /repos/${searchObj.owner}/${searchObj.repo}/issues`).then((res) => {
+        //     setSearchedRepo(res.data)
+        //     console.log(res.data)
+        // }).catch((error) => {
+        //     console.log(error.response.data.message)
+        // })
+        await octokit.paginate(octokit.rest.issues.listForRepo, searchObj)
+            .then((res) => {
+                const result = res || [];
+                if (result.length > 0) {
+                    console.log(result)
+                } else {
+                    console.log("존재하지 않는 레포지토리 입니다.")
+                }
+            }).catch((error) => {
+                console.log(error.response.data.message)
+            })
     }
 
     return (
--- src/pages/mainPage/presenter.js
+++ src/pages/mainPage/presenter.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import styled from "styled-components";
 
 const Presenter = (props) => {
@@ -14,33 +14,77 @@ const Presenter = (props) => {
 export default Presenter;
 
 const Search = (props) => {
-    const { searchObj, setSearchObj, repositoryFetch } = props
+
+    const { searchObj, setSearchObj, repositoryFetch, registeredRepo, setRegisterdRepo, } = props
+    const [displayFlag, setDisplayFlag] = useState(false)
+
     return (
-        <SearchSection>
-            <InputSection>
-                <SearchBox>
-                    <Label>user</Label>
-                    <Input
-                        onChange={(e) => {
-                            setSearchObj({ ...searchObj, owner: e.target.value })
-                        }}
-                    />
-                </SearchBox>
-                <SearchBox>
-                    <Label>repo</Label>
-                    <Input
-                        onChange={(e) => {
-                            setSearchObj({ ...searchObj, repo: e.target.value })
-                        }}
-                    />
-                </SearchBox>
-            </InputSection>
-            <SearchButton
-                onClick={() => { repositoryFetch() }}
-            >조회</SearchButton>
-        </SearchSection>
+        <Fragment>
+            <SearchSection>
+                <InputSection>
+                    <SearchBox>
+                        <Label>user</Label>
+                        <Input
+                            value={searchObj.owner}
+                            onChange={(e) => {
+                                setSearchObj({ ...searchObj, owner: e.target.value })
+                            }}
+                        />
+                    </SearchBox>
+                    <SearchBox>
+                        <Label>repo</Label>
+                        <Input
+                            value={searchObj.repo}
+                            onChange={(e) => {
+                                setSearchObj({ ...searchObj, repo: e.target.value })
+                            }}
+                        />
+                    </SearchBox>
+                    <SelectionBox>
+                        <TittleBox>
+                            Selected Repository List<span onClick={() => { setDisplayFlag(!displayFlag) }} style={{ color: "#D1D1D1" }}>{`>`}</span></TittleBox>
+                        <FullBox display={displayFlag}>
+                            <HalfBox middleLine={true}>
+                                <SelectBox
+                                    onClick={() => {
+                                        const coppiedRegisteredRepo = [...registeredRepo]
+                                        coppiedRegisteredRepo.push(searchObj)
+                                        let result = [...new Set(coppiedRegisteredRepo)]
+                                        if (result.length < 5) {
+                                            setRegisterdRepo(result)
+                                            localStorage.setItem("registeredRepo", JSON.stringify(result));
+                                        }
+                                        else {
+                                            alert("등록할 수 있는 레포지토리의 최대개수는 4개 까지입니다.")
+                                            return;
+                                        }
+                                    }}>{searchObj.owner} / {searchObj.repo}{<span style={{ color: "#A6CDFF" }}>{`>`}</span>}</SelectBox>
+                            </HalfBox>
+                            <HalfBox>
+                                {registeredRepo?.map((e) => {
+                                    return (
+                                        <SelectedBox
+                                            onClick={() => {
+                                                const coppiedRegisteredRepo = [...registeredRepo]
+                                                const filtered = coppiedRegisteredRepo.filter((value) => { return value != e })
+                                                let result = [...new Set(filtered)]
+                                                setRegisterdRepo(result)
+                                                localStorage.setItem("registeredRepo", JSON.stringify(result));
+                                            }}> {e.owner} / {e.repo}{<span style={{ color: "#FF0000" }}>{`X`}</span>}
+                                        </SelectedBox>
+                                    )
+                                })}
+                            </HalfBox>
+                        </FullBox>
+                    </SelectionBox>
+                </InputSection>
+                <SearchButton
+                    onClick={() => { repositoryFetch() }}
+                >조회</SearchButton>
+            </SearchSection>
+        </Fragment>
     )
-}
+};
 
 const Backgroud = styled.div`
     background-color: white;
@@ -51,10 +95,11 @@ const Backgroud = styled.div`
 const SearchSection = styled.div`
     display: flex;
     background-color: white;
+    width: 100%;
     height: 65px;
     justify-content: space-between;
     box-sizing: border-box;
-    position: relative;
+    position: fixed;
     margin-top: 20px;
     padding: 0px 20px 0px 20px;
 `;
@@ -66,23 +111,92 @@ const InputSection = styled.div`
 const SearchBox = styled.div`
     display: flex;
     flex-direction: column;
-    height: 100%;
 `
 
 const Label = styled.label`
     display: flex;
-    justify-content: center;
-    height: 30px;
+    height: 20px;
     align-items: center;
+    padding-left: 10px;
 `;
 
 const Input = styled.input`
     width: 200px;
-    height: 70px;
-    background-color: rebeccapurple;
+    flex: 1;
+    background-color: white;
+    border: 1px solid black;
+    padding-left: 10px;
 `;
 
 const SearchButton = styled.button`
     width: 100px;
     height: auto;
+`;
+
+const SelectionBox = styled.div`
+    border: 1px solid #D0D4D9;
+    width: 400px;
+    box-sizing: border-box;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    margin-left: 50px;
+`;
+
+const FullBox = styled.div`
+    ${({ display }) => {
+        return display ? `display: flex;` : `display: none;`
+    }} 
+    justify-content: center;
+    height: 250px
+`;
+
+const TittleBox = styled.div`
+    border-bottom: 1px solid rgb(208, 212, 217);
+    height: 50px;
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    color: rgb(208, 212, 217);
+    justify-content: space-between;
+    cursor: pointer;
+`;
+
+const HalfBox = styled.div`
+    width: 50%;
+    overflow: auto;
+    padding: 5px;
+    &::-webkit-scrollbar {
+        display: none;
+        -ms-overflow-style: none;
+    }
+    ${({ middleLine }) => {
+        return middleLine ? `border-right: 1px solid #D0D4D9` : null;
+    }}
+`;
+
+const SelectBox = styled.div`
+    border: 1px solid #A6CDFF;
+    color: #A6CDFF;
+    margin-bottom: 5px;
+    border-radius: 10px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    padding: 0px 10px 0px 10px;
+    justify-content: space-between;
+    cursor: pointer;
+`;
+
+const SelectedBox = styled.div`
+    border: 1px solid #D0D4D9;
+    background-color: #EFEFEF;
+    color: black;
+    margin-bottom: 5px;
+    border-radius: 10px;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    padding: 0px 10px 0px 10px;
+    justify-content: space-between;
+    cursor: pointer;
 `;
